ab34f166769b7592e1c481311cb1cd12e7fe7ed2
Athul Vijayan <athulvijayan6@gmail.com>
2014-04-03 07:59:52 +0530

    made data one-one

--- miniProject_1/miniproject.m
+++ miniProject_1/miniproject.m
@@ -3,9 +3,19 @@ load('GPSdata.mat');
 %convert to radians
 lat = degtorad(Locationdata(:,2));
 lon = degtorad(Locationdata(:,3));
+time = Locationdata(:,1);
+
+accuracy = Locationdata(:,4);
+clear('Locationdata');
 
 Speed = convvel(Speed,'km/h','m/s');        %convert measured speed to m/s
 
+% In the data it is evident that different GPS locations are data present for the same same timestamp
+% As this is not physically possible, it is caused by error. So we assign mean of Location data to such time stamps.
+[time, ia, ic] = unique(time);
+lat = lat(ia);
+lon = lon(ia);
+clear('ia','ic');
 [xRaw, yRaw, zRaw] = geodeticToCartesian(lat,lon,0);   % transformed coordinates in cartesian
 
 % Centralize the data
@@ -22,4 +32,3 @@ clear('xRaw','yRaw','zRaw');   % free up the computer memory from unused variabl
 
 % For denoising PCA can be used
 
-	
